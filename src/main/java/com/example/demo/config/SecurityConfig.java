5ca77ecc8f4e0019d228ef58df4ea2ac1765df87
13928421127 <singkingcho@gmail.com>
2020-12-30 14:22:18 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @author 邱道长
+ * 2020/12/30
+ */
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.httpBasic();
+    }
+}
